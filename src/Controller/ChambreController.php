b55a04dd88dea3829f342a1f268f913d1b1f8608
cpailler <aprel.illectemn@gmail.com>
2018-04-20 17:25:30 +0200

    mise en place du système de sélection des chambres

--- src/Controller/ChambreController.php
+++ src/Controller/ChambreController.php
@@ -24,12 +24,14 @@ class ChambreController extends AbstractController
     {
         $chambreManager = new ChambreManager();
         $chambres = $chambreManager->findAll();
-        $datas = [];
+        $datasorigin = [];
+        $error=false;
+        $select=["","","",false];
         $i=0;
         foreach ($chambres as $diapo => $data) {
-            if (!isset($datas[$data['chambres_id']])) {
+            if (!isset($datasorigin[$data['chambres_id']])) {
                 $i++;
-                $datas += [$i => array(
+                $datasorigin += [$i => array(
                     'id' => $data['chambres_id'],
                     'titre' => $data['titre'],
                     'texte' => $data['texte'],
@@ -42,14 +44,42 @@ class ChambreController extends AbstractController
 
                 } else {
 
-                $datas[$data['chambres_id']]['urlImage']+= [$data['id'] => $data['urlImage']];
+                $datasorigin[$data['chambres_id']]['urlImage']+= [$data['id'] => $data['urlImage']];
 
             }
+
+        }
+        $datas=$datasorigin;
+        $i=0;
+        if (isset($_POST['submit'])){
+
+            $select[0]=$_POST['style'];
+            $select[1]=$_POST['salleDeBain'];
+            $select[2]=$_POST['literie'];
+            if (isset($_POST['PMR'])){
+                $select[3]=true;
             }
+
+            $datas=[];
+            foreach ($datasorigin as $data){
+                if ((($_POST['style']=='')||($_POST['style']==$data['style']))&&
+                    (($_POST['salleDeBain']=='')||($_POST['salleDeBain']==$data['salleDeBain']))&&
+                    (($_POST['literie']=='')||($_POST['literie']==$data['literie']))&&
+                    ((isset($_POST['PMR'])&&($data['accessibilite']=='Oui'))||(!isset($_POST['PMR'])))){
+                    $i++;
+                    $datas+=[$i=>$data];
+                }
+            }
+            if (empty($datas)){
+                $datas=$datasorigin;
+                $error=true;
+            }
+        }
+
 
             //var_dump($datas[$data['chambres_id']]['urlImage']);
 
-        return $this->twig->render('Chambres/Chambres.html.twig', ['chambres' => $chambres, 'datas' => $datas]);
+        return $this->twig->render('Chambres/Chambres.html.twig', ['chambres' => $chambres, 'datas' => $datas, 'error'=> $error, 'select'=> $select]);
 
     }
 
