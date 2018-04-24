879413b8e90be417e939ed8778e60607b568418d
Tiffanym <tiffany.maurath@orange.fr>
2018-04-24 21:03:01 +0200

    Création de la page d'administration de la page d'accueil, non terminée, un mystère à résoudre concernant la modification des images

--- src/Controller/AdministrationController.php
+++ src/Controller/AdministrationController.php
@@ -9,6 +9,8 @@
 namespace Controller;
 
 use Controller\Date\Month;
+use Model\AccueilManager;
+use Model\Accueil;
 use Model\ChambreManager;
 use Model\DiapoChambreManager;
 use Model\ReservationManager;
@@ -29,14 +31,13 @@ class AdministrationController extends AbstractController
     public function index()
     {
         session_start();
-        if (!isset($_SESSION['user']) && !isset($_SESSION['password']) ) {
+        if (!isset($_SESSION['user']) && !isset($_SESSION['password'])) {
             header('Location: /Login');
         }
 
 
         return $this->twig->render('Administration/Administration.html.twig');
     }
-
 
 
     public function DiapoAccueil()
@@ -72,7 +73,7 @@ class AdministrationController extends AbstractController
                             $cheminEtNomTemporaire = $_FILES['file']['tmp_name'][$i];
 
                             // Nouvel emplacement du fichier :
-                            $deplacementOK = move_uploaded_file($cheminEtNomTemporaire, substr($uniqueId,1,strlen($uniqueId)));
+                            $deplacementOK = move_uploaded_file($cheminEtNomTemporaire, substr($uniqueId, 1, strlen($uniqueId)));
 
 
                             //Création de l'élément dans la base de donnée
@@ -81,7 +82,7 @@ class AdministrationController extends AbstractController
                             var_dump($ok);
 
                             if ($deplacementOK) {
-                                $message .= "Upload OK : " . $uniqueId ;
+                                $message .= "Upload OK : " . $uniqueId;
                             } else {
                                 $message .= "Pas upload";
                             }
@@ -99,10 +100,10 @@ class AdministrationController extends AbstractController
 
         $diapos = $diapoManager->findAll();
 
-        foreach ($diapos as $diapo){
+        foreach ($diapos as $diapo) {
             if (isset($_POST[$diapo['id']])) {
                 $diapoManager->delete($diapo['id']);
-                $path = substr($diapo['urlImage'],1,strlen($diapo['urlImage']));
+                $path = substr($diapo['urlImage'], 1, strlen($diapo['urlImage']));
                 unlink($path);
             }
 
@@ -110,59 +111,58 @@ class AdministrationController extends AbstractController
 
         $diapos = $diapoManager->findAll();
 
-            return $this->twig->render('Administration/DiapoAccueilAdmin.html.twig', ['diapos' => $diapos, 'message' => $message]);
+        return $this->twig->render('Administration/DiapoAccueilAdmin.html.twig', ['diapos' => $diapos, 'message' => $message]);
     }
 
-    public function AdminChambres(){
+    public function AdminChambres()
+    {
 
         $chambreManager = new ChambreManager();
         $diapoChambreManager = new DiapoChambreManager();
-        $diapos=$diapoChambreManager->findAll();
-        $select="";
-        $chambreAModifier=[];
+        $diapos = $diapoChambreManager->findAll();
+        $select = "";
+        $chambreAModifier = [];
 
-        $error=[];
-        $valid=[];
-        foreach ($diapos as $diapo){
+        $error = [];
+        $valid = [];
+        foreach ($diapos as $diapo) {
             if (isset($_POST[$diapo['id']])) {
                 $diapoChambreManager->delete($diapo['id']);
-                $path = substr($diapo['urlImage'],1,strlen($diapo['urlImage']));
+                $path = substr($diapo['urlImage'], 1, strlen($diapo['urlImage']));
                 unlink($path);
-                $valid[]="Image supprimée avec succès";
+                $valid[] = "Image supprimée avec succès";
             }
 
         }
-        $diapos=[];
+        $diapos = [];
         //Selection de la chambre
-        if (isset($_POST['chambre'])){
+        if (isset($_POST['chambre'])) {
             $select = $_POST['chambre'];
             $chambreAModifier = $chambreManager->findOneById($_POST['chambre']);
             $diapos = $diapoChambreManager->findByChambreId($_POST['chambre']);
         }
-        if (isset($_POST['modifier'])){
+        if (isset($_POST['modifier'])) {
             var_dump($_POST);
             $data = [];
-            $id=$_POST['id'];
-            $data['titre']=$_POST['titre'];
-            $data['prix']=$_POST['prix'];
-            $data['texte']=$_POST['texte'];
-            $data['style']=$_POST['style'];
-            $data['salleDeBain']=$_POST['salleDeBain'];
-            $data['literie']=$_POST['literie'];
-            if (isset($_POST['PMR'])){
-                $data['accessibilite']='Oui';
-            }
-            else{
-                $data['accessibilite']='Non';
+            $id = $_POST['id'];
+            $data['titre'] = $_POST['titre'];
+            $data['prix'] = $_POST['prix'];
+            $data['texte'] = $_POST['texte'];
+            $data['style'] = $_POST['style'];
+            $data['salleDeBain'] = $_POST['salleDeBain'];
+            $data['literie'] = $_POST['literie'];
+            if (isset($_POST['PMR'])) {
+                $data['accessibilite'] = 'Oui';
+            } else {
+                $data['accessibilite'] = 'Non';
             }
             var_dump($data);
-            $ok = $chambreManager->update($id,$data);
+            $ok = $chambreManager->update($id, $data);
             var_dump($ok);
-            if (!$ok){
-                $error[]="La base de donnée a refusé votre requête, veuillez contacter votre support technique.";
-            }
-            else {
-                $valid[]="Chambre modifiée avec succès";
+            if (!$ok) {
+                $error[] = "La base de donnée a refusé votre requête, veuillez contacter votre support technique.";
+            } else {
+                $valid[] = "Chambre modifiée avec succès";
             }
 
             //upload des images
@@ -180,7 +180,7 @@ class AdministrationController extends AbstractController
                         $size = filesize($_FILES['nouvellesDiapos']['tmp_name'][$i]);
 
                         if ($size > 2000000) {
-                            $error[]= "Le fichier ne doit pas dépasser 2Mo";
+                            $error[] = "Le fichier ne doit pas dépasser 2Mo";
                         } else {
                             // Récupération de l'extension du fichier (fin de la chaîne) :
                             $extensionDuFichier = strrchr($_FILES['nouvellesDiapos']['name'][$i], ".");
@@ -192,44 +192,106 @@ class AdministrationController extends AbstractController
                             $cheminEtNomTemporaire = $_FILES['nouvellesDiapos']['tmp_name'][$i];
 
                             // Nouvel emplacement du fichier :
-                            $deplacementOK = move_uploaded_file($cheminEtNomTemporaire, substr($uniqueId,1,strlen($uniqueId)));
+                            $deplacementOK = move_uploaded_file($cheminEtNomTemporaire, substr($uniqueId, 1, strlen($uniqueId)));
                             if ($deplacementOK) {
-                                $ok = $diapoChambreManager->insert(['urlImage' => $uniqueId,'chambres_id'=>$id]);
+                                $ok = $diapoChambreManager->insert(['urlImage' => $uniqueId, 'chambres_id' => $id]);
                                 if ($ok) {
                                     $valid[] = "Image ajoutée avec succès ";
                                 } else {
-                                    unlink(substr($uniqueId,1,strlen($uniqueId)));
+                                    unlink(substr($uniqueId, 1, strlen($uniqueId)));
                                     $error[] = "Votre image n'as pas pu être enregistrée, si l'erreur persiste, veuillez contacter le service technique";
                                 }
                             }
 
                         }
                     } else {
-                        $error[]= "Format d'image non supporté (formats supportés : jpeg, jpg, png, gif)";
+                        $error[] = "Format d'image non supporté (formats supportés : jpeg, jpg, png, gif)";
                     }
                 }
             }
         }
-        if (isset($_POST['delete'])){
+        if (isset($_POST['delete'])) {
             $ok = $chambreManager->delete($_POST['id']);
-            if (!$ok){
-                $error[]="La chambre n'as pas pu être supprimée";
-            }
-            else{
-                $valid[]="La chambre a été supprimée avec succès";
+            if (!$ok) {
+                $error[] = "La chambre n'as pas pu être supprimée";
+            } else {
+                $valid[] = "La chambre a été supprimée avec succès";
             }
         }
 
         $chambres = $chambreManager->findAll();
 
 
-        return $this->twig->render('Administration/ChambresAdmin.html.twig',[
-            'select'=>$select,
-            'chambres'=>$chambres,
-            'chambreAModifier'=>$chambreAModifier,
-            'diapos'=>$diapos,
-            'errors'=>$error,
-            'valids'=>$valid]);
+        return $this->twig->render('Administration/ChambresAdmin.html.twig', [
+            'select' => $select,
+            'chambres' => $chambres,
+            'chambreAModifier' => $chambreAModifier,
+            'diapos' => $diapos,
+            'errors' => $error,
+            'valids' => $valid]);
+    }
+
+    public function ArticlesAccueil()
+    {
+
+        $accueilManager = new AccueilManager();
+        $accueil = $accueilManager->findAll();
+
+        $error = [];
+        $valid = [];
+        if (isset($_POST['modifier'])) {
+            $data = [];
+            $id = $_POST['id'];
+            $data['titre'] = $_POST['titre'];
+            $data['texte'] = $_POST['texte'];
+
+            // Vérifie qu'un fichier a bien été envoyé
+            if (!empty($_FILES['urlImage']['name'])) {
+                var_dump($_FILES['urlImage']);
+                // Vérification du type de fichier
+                $typeMime = mime_content_type($_FILES['urlImage']['tmp_name']);
+                if (in_array($typeMime, ['image/jpeg', 'image/png', 'image/jpg', 'image/gif'])) {
+
+                    // Vérification taille du fichier
+                    $size = filesize($_FILES['urlImage']['tmp_name']);
+
+                    if ($size > 5000000) {
+                        $error[] = "Le fichier ne doit pas dépasser 5Mo";
+                    } else {
+                        // Récupération de l'extension du fichier (fin de la chaîne) :
+                        $extensionDuFichier = strrchr($_FILES['urlImage']['name'], ".");
+
+                        // Définition d'un nom unique :
+                        $uniqueId = uniqid('/assets/images/image') . $extensionDuFichier;
+
+                        // Emplacement temporaire du fichier uploadé :
+                        $cheminEtNomTemporaire = $_FILES['urlImage']['tmp_name'];
+
+                        // Nouvel emplacement du fichier :
+                        $deplacementOK = move_uploaded_file($cheminEtNomTemporaire, substr($uniqueId, 1, strlen($uniqueId)));
+
+                        $data['urlImage'] = $uniqueId;
+
+                    }
+                } else {
+                    $error[] = "Format d'image non supporté (formats supportés : jpeg, jpg, png, gif)";
+                }
+
+            } else {
+                $data['urlImage'] = $_FILES['urlImage'];
+            }
+
+            $ok = $accueilManager->update($id, $data);
+            if (!$ok) {
+                $error[] = "La base de donnée a refusé votre requête, veuillez contacter votre support technique.";
+            } else {
+                $valid[] = "Article modifiée avec succès";
+            }
+        }
+
+        $accueil = $accueilManager->findAll();
+
+        return $this->twig->render('Administration/ArticlesAccueilAdmin.html.twig', ['accueil' => $accueil]);
     }
 
 }
