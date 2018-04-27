61178defae803026450c87e8dd5590606c78a88a
Tiffanym <tiffany.maurath@orange.fr>
2018-04-27 12:34:48 +0200

    CSS page de réservation

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -91,6 +91,12 @@ class ReservationController extends AbstractController
             else {
                 $errors[]="Veuillez saisir votre nom";
             }
+            if ($_POST['prenomClient']!=""){
+                $data['prenomClient']=$_POST['nomClient'];
+            }
+            else {
+                $errors[]="Veuillez saisir votre prénom";
+            }
             if ($_POST['mailClient']!=""){
                 $data['mailClient']=$_POST['mailClient'];
             }
